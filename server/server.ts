bd505ed3ce2860a7d5ec9cd3f24fb2a0d9c943e1
Kim Chiu <kimchiux123@gmail.com>
2021-03-02 22:54:13 -0800

    changed module to commonjs in tsconfig

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 // const express = require('express');
 // const path = require('path');
 
@@ -22,4 +22,4 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
 
-module.exports = app;
+// module.exports = app;
